0f4edf870310da6a23caab5bea9bdfcc2609f342
CaioNunes3000 <caionunes3000@gmail.com>
2018-12-03 01:25:53 -0300

    caindo

--- codigo.js
+++ codigo.js
@@ -1,5 +1,7 @@
 var wrapper =  document.querySelector('#wrapper');
-
+var clock = document.querySelector('#clock');
+var chair = document.querySelector('#chair');
+var alice = document.querySelector('#alice');
 
 wrapper.addEventListener('click', (event) => {
 
@@ -9,10 +11,16 @@ wrapper.addEventListener('click', (event) => {
 
 	if(event.target.classList.contains('ancora-grupo')) {
 		wrapper.style = 'transform: translateY(-40%)';
+		clock.style = 'transform: translateY(-30%) scale(0.5) rotate(30deg)';
+		chair.style = 'transform: translateY(-30%) scale(0.5) rotate(-45deg)';
+		alice.style = 'transform: translateY(150%) scale(0.5)';
 	}
 
 	if(event.target.classList.contains('ancora-contato')) {
 		wrapper.style = 'transform: translateY(-80%)';
+		clock.style = 'transform: translateY(-30%) scale(0.5) rotate(15deg)';
+		chair.style = 'transform: translateY(-30%) scale(0.5) rotate(-45deg)';
+		alice.style = 'transform: translateY(100%) scale(0.5)';
 	}
 
 });
